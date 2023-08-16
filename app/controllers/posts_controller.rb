c0f69d2d7e9942a77f297c2d62b0120f950b8177
Jibran-Arshad-Zubair <ranajibranarshad@gmail.com>
2023-08-16 17:06:29 +0500

    CRUD-Update Methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,11 +22,17 @@ class PostsController < ApplicationController
     end 
 
     def edit
-
+        @post = Post.find(params[:id])
     end 
 
     def update 
+        @post = Post.find(params[:id])
 
+        if @post.update(post_params)
+            redirect_to posts_path, :notice => "Your Post has been Updated "
+            else
+               render "Edit"
+            end  
     end 
 
     def destroy
